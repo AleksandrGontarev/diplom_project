59e8680f718f3a362d02e40dc5dd67bb6a1a6641
Gontarev <gontarevsanya@gmail.com>
2022-09-18 10:03:05 +0300

    in warehouse add admin books order

--- warehouse/orders/admin.py
+++ warehouse/orders/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user_email', 'status', 'delivery_address', 'order_id_in_shop')
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order_id', 'book_store_id', 'quantity')
